f7230417d1894dbcd6db267753347896b9194846
kovlanproj <kozorizva@gmail.com>
2022-11-14 12:47:07 +0200

    add changes

--- src/components/Gallery/ImageGallery/ImageGallery.jsx
+++ src/components/Gallery/ImageGallery/ImageGallery.jsx
@@ -7,102 +7,100 @@ import { Loader } from 'components/Loader/Loader';
 import { Button } from '../../Button/Button';
 
 function smoothScroll() {
-    const cardHeight = document
-        .querySelector('ul')
-        .firstElementChild.getBoundingClientRect().height;
+  const cardHeight = document
+    .querySelector('ul')
+    .firstElementChild.getBoundingClientRect().height;
 
-    window.scrollBy({
-        top: cardHeight * 2,
-        behavior: 'smooth',
-    });
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
 }
 
 export class ImageGallery extends Component {
-    state = {
-        query: '',
+  state = {
+    query: '',
+    images: [],
+    totalHits: 0,
+    isLoading: false,
+    page: 1,
+    error: false,
+    visibleBtn: false,
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const prevQuery = prevProps.formQuery;
+    const nextQuery = this.props.formQuery;
+    const prevPage = prevState.page;
+    const nextPage = this.state.page;
+    const { images, visibleBtn, totalHits, query } = this.state;
+    const prevStateQuery = prevState.query;
+
+    if (prevQuery !== nextQuery) {
+      this.setState(prev => ({
+        ...prev,
         images: [],
-        totalHits: 0,
-        isLoading: false,
         page: 1,
-        error: false,
-        visibleBtn: false,
-    };
+        query: this.props.formQuery,
+      }));
+    }
 
-    async componentDidUpdate(prevProps, prevState) {
-        const prevQuery = prevProps.formQuery;
-        const nextQuery = this.props.formQuery;
-        const prevPage = prevState.page;
-        const nextPage = this.state.page;
-        const { images, visibleBtn, totalHits, query } = this.state;
-        const prevStateQuery = prevState.query;
-
-        if (prevQuery !== nextQuery) {
-            this.setState(prev => ({
-                ...prev,
-                images: [],
-                page: 1,
-                query: this.props.formQuery,
-            }));
+    if (prevStateQuery !== query || (prevPage !== nextPage && nextPage !== 1)) {
+      try {
+        this.setState({ isLoading: true });
+        console.log('query -', nextQuery, '   page - ', nextPage);
+        const imageList = await getImages(nextQuery, nextPage);
+        if (imageList.totalHits === 0) {
+          alert('Images not found');
         }
-
-        if (
-            prevStateQuery !== query ||
-            (prevPage !== nextPage && nextPage !== 1)
-        ) {
-            try {
-                this.setState({ isLoading: true });
-                console.log('query -', nextQuery, '   page - ', nextPage);
-                const imageList = await getImages(nextQuery, nextPage);
-                if (imageList.totalHits === 0) {
-                    alert('Images not found');
-                }
-                this.setState(state => ({
-                    images: [...state.images, ...imageList.hits],
-                    isLoading: false,
-                    totalHits: imageList.totalHits,
-                }));
-            } catch (error) {
-                this.setState({ error: true, isLoading: false });
-                console.log(error);
+        this.setState(
+          state => ({
+            images: [...state.images, ...imageList.hits],
+            isLoading: false,
+            totalHits: imageList.totalHits,
+          }),
+          () => {
+            if (nextPage !== 1) {
+              smoothScroll();
             }
-        }
-
-        if (images.length !== 0 && !visibleBtn && images.length < totalHits) {
-            this.setState({ visibleBtn: true });
-        } else if (images.length >= totalHits && visibleBtn) {
-            this.setState({ visibleBtn: false });
-        }
-
-        if (prevPage !== nextPage && nextPage !== 1) {
-            setTimeout(() => {
-                smoothScroll();
-            }, 0);
-        }
-    }
-
-    onClickLoadMoreBtn = () => {
-        this.setState(prevState => ({
-            page: prevState.page + 1,
-        }));
-    };
-
-    render() {
-        const { images, isLoading, visibleBtn } = this.state;
-
-        return (
-            <>
-                <ImageGalleryList>
-                    {images.map(image => (
-                        <ImageGalleryItem image={image} key={image.id} />
-                    ))}
-                </ImageGalleryList>
-                {isLoading && <Loader />}
-                {visibleBtn && <Button onClick={this.onClickLoadMoreBtn} />}
-            </>
+          }
         );
+      } catch (error) {
+        this.setState({ error: true, isLoading: false });
+        console.log(error);
+      }
     }
+
+    if (images.length !== 0 && !visibleBtn && images.length < totalHits) {
+      this.setState({ visibleBtn: true });
+    } else if (images.length >= totalHits && visibleBtn) {
+      this.setState({ visibleBtn: false });
+    }
+  }
+
+  onClickLoadMoreBtn = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
+  };
+
+  render() {
+    const { images, isLoading, visibleBtn } = this.state;
+
+    return (
+      <>
+        <ImageGalleryList>
+          {images.map(image => (
+            <ImageGalleryItem image={image} key={image.id} />
+          ))}
+        </ImageGalleryList>
+        {isLoading && <Loader />}
+        {visibleBtn && <Button onClick={this.onClickLoadMoreBtn} />}
+      </>
+    );
+  }
 }
 
 ImageGallery.propTypes = {
-    formQuery: PropTypes.string.isRequired,
+  formQuery: PropTypes.string.isRequired,
 };
