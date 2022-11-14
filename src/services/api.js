c5635822dd6f1c5243997ac86702a1f0962b94bf
kovlanproj <kozorizva@gmail.com>
2022-11-14 22:11:21 +0200

    change axios

--- src/services/api.js
+++ src/services/api.js
@@ -1,20 +1,24 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api';
 const API_KEY = '28916923-d414eade46dc2c6542ccb3cb8';
 const IMAGES_PER_PAGE = 12;
 
+axios.defaults.baseURL = BASE_URL;
+axios.defaults.params = {
+  key: API_KEY,
+  image_type: 'photo',
+  orientation: 'horizontal',
+  per_page: IMAGES_PER_PAGE,
+};
+
 export const getImages = async (query, page) => {
-    const config = {
-        params: {
-            q: query,
-            page: page,
-            key: API_KEY,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            per_page: IMAGES_PER_PAGE,
-        },
-    };
-    const response = await axios.get(BASE_URL, config);
-    return response.data;
+  const config = {
+    params: {
+      q: query,
+      page: page,
+    },
+  };
+  const response = await axios.get('/', config);
+  return response.data;
 };
